ce15bb54440dd18a9527f69f8b611cefaf08b9ae
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-03-20 12:26:09 -0300

    feat: add Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {Image} from 'react-native';
 
-import {Container, Text} from './styles';
+import {
+  Container,
+  SignInButton,
+  SignInButtonText,
+  RegisterText,
+  RegisterButton,
+  RegisterButtonText,
+} from './styles';
+
+// TODO: turn into a ES6 import
+const heroImg = require('../../../assets/images/hero.png');
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Text>Hello, Tinsports!</Text>
+      <Image source={heroImg} />
+      <SignInButton activeOpacity={0.8}>
+        <SignInButtonText>Entrar</SignInButtonText>
+      </SignInButton>
+
+      <RegisterText>Não tem conta?</RegisterText>
+      <RegisterButton activeOpacity={0.6}>
+        <RegisterButtonText>Inscreva-se</RegisterButtonText>
+      </RegisterButton>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,9 +4,32 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: #dbd9ff;
 `;
 
-export const Text = styled.Text`
+export const SignInButton = styled.TouchableOpacity`
+  align-items: center;
+  background-color: #007e33;
+  margin-top: 32px;
+  padding: 8px 0;
+  width: 70%;
+  border-radius: 160px;
+`;
+
+export const SignInButtonText = styled.Text`
   font-size: 24px;
-  color: #000;
+  color: #fff;
+`;
+
+export const RegisterText = styled.Text`
+  font-size: 16px;
+  margin: 24px 0 8px;
+`;
+
+export const RegisterButton = styled.TouchableOpacity``;
+
+export const RegisterButtonText = styled.Text`
+  color: #281947;
+  font-size: 28px;
+  text-decoration: underline;
 `;
