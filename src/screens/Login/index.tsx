4380381391287fa7b08a7e21b2b627e747471f13
JeanCDev <jeangames15@gmail.com>
2021-03-20 19:04:53 -0300

    Login Page

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 
-import {Container} from './styles';
+import {ButtonView, Container, Input, Label, SignInButton, SignInButtonText} from './styles';
+
+//const [email, setEmail] = useState('');
+//const [password, setPassword] = useState('');
 
 const Login: React.FC = () => {
-  return <Container />;
+  return (
+
+    <Container>
+      <Label>Email</Label>
+      <Input placeholder="Digite seu email" />{/* value={email} onChangeText={setEmail}/> */}
+
+      <Label>Senha</Label>
+      <Input placeholder="Digite sua senha" />{/* value={password} onChangeText={setPassword}/> */}
+      
+      <ButtonView>
+        <SignInButton>
+          <SignInButtonText>Entrar</SignInButtonText>
+        </SignInButton>
+      </ButtonView>
+    </Container>
+    
+  );
 };
 
 export default Login;
